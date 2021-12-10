3defdc5de4fbabeefc92c675f7d9dfe059964a3f
Alina Gan <alina.gan@icloud.com>
2021-12-10 14:54:43 +0700

    Classes

---
+++ CConnector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGZ_POGI3_
+{
+    public abstract class CConnector
+    {
+        string path;
+
+        public CConnector()
+        {
+        }
+
+        public CConnector(string path)
+        {
+            this.path = path;
+        }
+
+        public void SetPath(string path)
+        {
+            this.path = path;
+        }
+
+        public string GetPath()
+        {
+            return path;
+        }
+
+        public abstract bool SaveData(CDataBase db);
+        public abstract CDataBase LoadData();
+    }
+}
