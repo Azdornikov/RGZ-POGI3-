32f7b0e477902750fff08c2c883b374fdb684575
Andrew Zdornikov <Azdornikov@yandex.ru>
2021-12-10 15:05:20 +0700

    add CGenerator and classes for db

--- CConnector.cs
+++ CConnector.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace RGZ_POGI3_
 {
-    class CConnector
+    public abstract class CConnector
     {
+        string path;
+
+        public CConnector()
+        {
+        }
+
+        public CConnector(string path)
+        {
+            this.path = path;
+        }
+
+        public void SetPath(string path)
+        {
+            this.path = path;
+        }
+
+        public string GetPath()
+        {
+            return path;
+        }
+
+        public abstract bool SaveData(CDataBase db);
+        public abstract CDataBase LoadData();
     }
 }
---
+++ CGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGZ_POGI3_
+{
+    public class CGenerator
+    {
+        int[,] field;
+        Random rnd = new Random();
+
+        List<int> cards = new List<int>()
+        {
+            1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
+            11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19, 20, 20
+        };
+
+        public CGenerator(int weidth, int height)
+        {
+            field = new int[weidth, height];
+        }
+
+        public void generate()
+        {
+            int number;
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    number = rnd.Next(0, cards.Count);
+                    field[i, j] = cards[number];
+
+                    cards.RemoveAt(number);
+                }
+        }
+
+        public int getcell(int i, int j, int weidth, int height)
+        {
+            if (i < weidth && j < height)
+                return field[i, j];
+            return 0;
+        }
+    }
+}
