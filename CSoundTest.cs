73ec5c9d7d31141723e2ca3ceabbc2ee4b57cae4
Alina Gan <alina.gan@icloud.com>
2021-12-17 14:39:33 +0700

    test

---
+++ CSoundTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using NUnit.Framework;
+
+
+namespace RGZ_POGI3_
+{
+    [TestFixture]
+    class CSoundTest
+    {
+
+        [TestCase]
+        public void TestSound()
+        {
+            CSound zvuk = new CSound();
+            var exception = Assert.Throws<Exception>(() => zvuk.play(@"C:\RGZ(POGI3)\Cards\0.png"));
+            // сравнение полученного сообщения с ожидаемым
+            Assert.That(exception.Message, Is.EqualTo("???"));
+        }
+    }
+}
