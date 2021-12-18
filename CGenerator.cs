7d1289bc6903d235f5f944f88090d6baa9e0982c
Alina Gan <alina.gan@icloud.com>
2021-12-18 15:25:09 +0700

    add test Generator

--- CGenerator.cs
+++ CGenerator.cs
@@ -24,10 +24,13 @@ namespace RGZ_POGI3_
 
         public void generate()
         {
+
             int number;
             for (int i = 0; i < field.GetLength(0); i++)
                 for (int j = 0; j < field.GetLength(1); j++)
                 {
+                    if (cards.Count == 0) throw new Exception("Карточки исчерпаны");
+
                     number = rnd.Next(0, cards.Count);
                     field[i, j] = cards[number];
 
---
+++ SGeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RGZ_POGI3_
+{
+    [TestFixture]
+    class SGeneratorTest
+    {
+        [TestCase]
+        public void TestGenerator()
+        {
+            CGenerator c = new CGenerator(8, 8);
+
+            // получение исключения
+            var exception = Assert.Throws<Exception>(() => c.generate());
+            // сравнение полученного сообщения с ожидаемым
+            Assert.That(exception.Message, Is.EqualTo("Карточки исчерпаны"));
+
+        }
+    }
+}
